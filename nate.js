ffacefedcda8a41cf9fe9e202a27f8b0ba36a0ce
jungal01 <jungal01@luther.edu>
2016-12-09 17:42:10 -0600

    Update nate.js

--- nate.js
+++ nate.js
@@ -1,20 +1,4 @@
-changeLang = function() {
-  var dict = {
-    en: { : "<p>hello</p>", 
-    },
-    th: {"<p id='en'>hello</p>" : "<p id='th'>lorium ipsum</p>",
-    }
-  };
-  var enLang = 'en';
-  var thLang = 'th';
-  
-  if (document.querySelector('#en')) {
-    document.body.innerHTML = thLang;
-    document.getElementById('en').id = 'th';
-  }
-  
-  else if (document.querySelector('th')) {
-    document.body.innerHTML = enLang;
-    document.getElementById('th').id = 'en';
-  }
-}
+function clock() {
+  var time;
+  time = Date(year, month, hours, minutes, seconds);
+  body.document.getById('time').innerHTML = time;
